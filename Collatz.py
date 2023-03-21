03dd480448ed9fcec4976a00831460d74e463398
NexoNeko <meresidamente@hotmail.com>
2023-03-20 23:23:28 -0300

    Create Collatz.py

---
+++ Collatz.py
@@ -0,0 +1,12 @@
+def collatz(number):
+    while (True):
+        if number <= 1:
+            break;
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = 3 * number + 1
+        print(str(number))
+
+print('Enter number:')
+collatz(int(input()))
