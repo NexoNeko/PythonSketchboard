5f5c587129e745be3f9830bf2fad209db89779da
NexoNeko <meresidamente@hotmail.com>
2023-03-21 20:15:01 -0300

    Create print_inverted_ascii.py

---
+++ print_inverted_ascii.py
@@ -0,0 +1,18 @@
+grid = [
+    ['.', '.', '.', '.', '.', '.'],
+    ['.', 'O', 'O', '.', '.', '.'],
+    ['O', 'O', 'O', 'O', '.', '.'],
+    ['O', 'O', 'O', 'O', 'O', '.'],
+    ['.', 'O', 'O', 'O', 'O', 'O'],
+    ['O', 'O', 'O', 'O', 'O', '.'],
+    ['O', 'O', 'O', 'O', '.', '.'],
+    ['.', 'O', 'O', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.']]
+
+def print_inverted_ascii(grid):
+    for n in range(len(grid[0])):
+        for i in range(len(grid)):
+            print(grid[i][n], end='')
+        print(grid[i][n])
+
+print_inverted_ascii(grid)
