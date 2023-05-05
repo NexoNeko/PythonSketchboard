e0d04e85522d12eaf986207c5634135cbda16741
NexoNeko <meresidamente@hotmail.com>
2023-05-05 12:43:11 -0300

    stuff

---
+++ LLM prompting/Structured output.py
@@ -0,0 +1,22 @@
+import openai
+import os
+
+openai.api_key = os.getenv('OPENAIKEY')
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+prompt = f"""
+Generate a list of three made-up book titles along \ 
+with their authors and genres. 
+Provide them in JSON format with the following keys: 
+book_id, title, author, genre.
+"""
+response = get_completion(prompt)
+print(response)
